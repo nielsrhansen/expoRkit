9063ae266afcee642f4686b44e5b333e51c65f0a
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2012-10-03 16:19:10 +0200

    Adding Roxygen documentation.

--- R/expv.R
+++ R/expv.R
@@ -3,7 +3,6 @@
 ### classes from the Matrix package and SparseM package together with a
 ### method for ordinary matrices. 
 ###
-###
 ###     Copyright (C) 2012 Niels Richard Hansen.
 ###
 ### This program is free software; you can redistribute it and/or modify it
@@ -20,14 +19,79 @@
 ### along with this program; if not, a copy is available at
 ### http://www.r-project.org/Licenses/
 
-setGeneric("expv", function(x, v, ...) standardGeneric("expv"))
+##' Methods for computing the solution of the ODE \deqn{w'(t) = x w(t)
+##' + u} with initial condition \eqn{w(0) = v} at one or more time
+##' points. 
+##'
+##' Analytically the solution is given as \deqn{w(t) = \exp(tx)v + t
+##' \phi(tx)u}{w(t) = exp(tx)v + tphi(tx)u} with \eqn{\phi(z) =
+##' (\exp(z)-1)/z}{phi(z) = (exp(z)-1)/z}. For large matrices \eqn{x}
+##' the computation of the full matrices \eqn{\exp(tx)}{exp(tx)} and
+##' \eqn{\phi(tx)}{phi(tx)} is slow. An alternative is to compute the
+##' solution \eqn{w} directly for a given initial value \eqn{v}
+##' using Krylov subspace methods. This is, in particular, efficient
+##' for large sparse matrices.
+##'
+##' Note that if \eqn{Q} is a rate matrix for a homogeneous continuous
+##' time Markov process (non-negative off-diagonals and row-sums 0)
+##' and \eqn{v} is a probability vector (the initial distribution at
+##' time point 0), then the distribution at time point \eqn{t} solves
+##' \deqn{w'(t) = Q^T w(t).} In this case we want to take \eqn{x} to
+##' be \eqn{Q^T} to get the desired solution.
+##'
+##' The solution is computed using the Fortran package
+##' Expokit. Methods are available for classes implemented in the
+##' Matrix package as well as the SparseM package. The implementation
+##' avoids the computation of the full matrix exponential of \eqn{tx}
+##' and the approach is advantageous when we want to compute
+##' \eqn{w(t)} for one or a few initial values \eqn{v}. The full
+##' matrix exponential should \emph{not} be computed this way by
+##' looping over \eqn{n} different initial values.
+##' 
+##' Though there is a method implemented for ordinary (dense)
+##' matrices, such a matrix is simply coerced into a
+##' \code{CsparseMatrix} before the solution is computed. It is
+##' recommended that large sparse matrices are stored and handled as
+##' such, e.g. using the classes and methods from the Matrix
+##' package. Dense intermediates should be avoided.
+##' @title Matrix exponentiation and more. 
+##' @param x a matrix. 
+##' @param v a \code{numeric} vector. The initial value. 
+##' @param t a \code{numeric} vector of time points at which the
+##' solution is computed.
+##' @param u a \code{numeric} vector. 
+##' @param Markov \code{logical}. If \code{TRUE} the matrix is taken
+##' to be an intensity matrix and steps are taken to ensure that the
+##' computed result is a probability vector. Default \code{FALSE}.
+##' @param transpose \code{logical}. If \code{TRUE} transpose the
+##' matrix before the solution is computed. Default equals
+##' \code{Markov}.
+##' @param ... other arguments passed to \code{\link{Rexpv}}.
+##' @return An \eqn{n} by \eqn{k} matrix with \eqn{k} the length of
+##' \code{t} and \eqn{n} the dimension of the matrix \code{x}. The \eqn{i}'th
+##' column contains the solution of the ODE at time point \eqn{i}.
+##' @author Niels Richard Hansen \email{Niels.R.Hansen@@math.ku.dk}
+##' @seealso \code{\link{Rexpv}}, \code{\link[Matrix]{expm}},
+##' \code{\link[expm]{expm}}
+##' @export
+##' @docType methods
+##' @rdname expv-methods
+##' @exportMethod expv
+setGeneric("expv", function(x, v, t = 1.0, u = NULL,
+                            Markov = FALSE, transpose = Markov, ...)
+           standardGeneric("expv")
+           )
 
+##' @rdname expv-methods
+##' @aliases expv,matrix,vector-method
 setMethod("expv", signature("matrix", "vector"),
-          function(x, v, t = 1.0, u = NULL, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (is.numeric(x)) {
             ## For a numeric 'matrix' argument coerce the argument into a
             ## sparse matrix in CCS format.
-            w <- callGeneric(Matrix(x, sparse = TRUE), v = v, t = t, u = u, ...)
+            w <- callGeneric(Matrix(x, sparse = TRUE), v = v, t = t, u = u,
+                             Markov = Markov, transpose = transpose, ...)
           } else if (is.complex(x)) {
             ## For a complex 'matrix' the coercion to a sparse matrix
             ## is done manually as the Matrix package does not support
@@ -57,9 +121,13 @@ setMethod("expv", signature("matrix", "vector"),
           }
           )
 
+##' @rdname expv-methods
+##' @aliases expv,CsparseMatrix,vector-method
+##' @importClassesFrom Matrix CsparseMatrix
+##' @importMethodsFrom Matrix t
 setMethod("expv", signature("CsparseMatrix", "vector"),
-          function(x, v, t = 1.0, u = NULL, Markov = FALSE,
-                   transpose = Markov, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (transpose)
               x <- t(x)
             w <- matrix(0, length(v), length(t))
@@ -82,9 +150,12 @@ setMethod("expv", signature("CsparseMatrix", "vector"),
           }
           )
 
+##' @rdname expv-methods
+##' @aliases expv,TsparseMatrix,vector-method
+##' @importClassesFrom Matrix TsparseMatrix
 setMethod("expv", signature("TsparseMatrix", "vector"),
-          function(x, v, t = 1.0, u = NULL, Markov = FALSE,
-                   transpose = Markov, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (transpose)
               x <- t(x)
             w <- matrix(0, length(v), length(t))
@@ -107,9 +178,13 @@ setMethod("expv", signature("TsparseMatrix", "vector"),
           }
           )
 
+##' @rdname expv-methods
+##' @aliases expv,matrix.csc,vector-method
+##' @importClassesFrom SparseM matrix.csc
+##' @importMethodsFrom SparseM t
 setMethod("expv", signature("matrix.csc", "vector"),
-          function(x, v, t = 1.0, u = NULL, Markov = FALSE,
-                   transpose = Markov, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (transpose)
               x <- t(x)
             w <- matrix(0, length(v), length(t))
@@ -132,9 +207,12 @@ setMethod("expv", signature("matrix.csc", "vector"),
           }
           )
 
+##' @rdname expv-methods
+##' @aliases expv,matrix.csr,vector-method
+##' @importClassesFrom SparseM matrix.csr
 setMethod("expv", signature("matrix.csr", "vector"),
-          function(x, v, t = 1.0, u = NULL, Markov = FALSE,
-                   transpose = Markov, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (transpose)
               x <- t(x)
             w <- matrix(0, length(v), length(t))
@@ -157,9 +235,12 @@ setMethod("expv", signature("matrix.csr", "vector"),
           }
           )
 
+##' @rdname expv-methods
+##' @aliases expv,matrix.coo,vector-method
+##' @importClassesFrom SparseM matrix.coo
 setMethod("expv", signature("matrix.coo", "vector"),
-          function(x, v, t = 1.0, u = NULL, Markov = FALSE,
-                   transpose = Markov, ...) {
+          function(x, v, t = 1.0, u = NULL,
+                   Markov = FALSE, transpose = Markov, ...) {
             if (transpose)
               x <- t(x)
             w <- matrix(0, length(v), length(t))
@@ -174,7 +255,7 @@ setMethod("expv", signature("matrix.coo", "vector"),
                               u = u,
                               t = t[k],
                               Markov = Markov,
-                              storage = 'COO'
+                              storage = 'COO',
                               ...)
               v <- w[, k]
             }
